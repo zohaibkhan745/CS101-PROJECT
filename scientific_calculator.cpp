4d34f1deae0c120393135cecde6c46d6f9e8fcd2
zohaibkhan745 <152165821+zohaibkhan745@users.noreply.github.com>
2023-12-02 00:03:29 +0500

    Add files via upload

--- scientific_calculator.cpp
+++ scientific_calculator.cpp
@@ -1,26 +1,57 @@
-#include <iostream>
-#include <cmath>
+#include<iostream>
 using namespace std;
-
-// Function to calculate exponentiation
-double exponential(double base, double exponent)
-{
-    return pow(base, exponent);
+double add(){
+cout<<"enter your numbers "<<endl;
+int a, b;
+cin>>a>>b;
+return a+b;
 }
-
-int main() 
+double subtract(){
+    int a, b;
+    cout<<"enter your numbers "<<endl;
+    cin>>a>>b;
+    return a-b;
+}
+double multiply(){
+     int a, b;
+    cout<<"enter your numbers "<<endl;
+    cin>>a>>b;
+    return a*b;
+}
+double divide(){
+    double a, b;
+    cout<<"enter your numbers "<<endl;
+    cin>>a>>b;
+    if (b==0)
+    {
+        cout<<"math error";
+    }
+    return a/b;
+}
+int main()
 {
-    double base, exponent, result;
-
-    // Get input from the user
-    cout << "Enter base: ";
-    cin >> base;
-    cout << "Enter exponent: ";
-    cin >> exponent;
-
-    // Calculate and display the result
-    result = exponential(base, exponent);
-    cout << "Result: " << result << endl;
-
+    int operation=5;
+   cout<<"enter 1 for addition "<<endl;
+   cout<<"enter 2 for subtraction "<<endl;
+   cout<<"enter 3 for multiplication "<<endl;
+   cout<<"enter 4 for division "<<endl;
+   cin>>operation;
+   switch (operation)
+   {
+   case 1:
+    cout<<add();
+    break;
+   case 2:
+   cout<<subtract();
+   break;
+   case 3:
+   cout<<multiply();
+   break;
+   case 4:
+   cout<<divide();
+   break;
+   default:
+    break;
+   }
     return 0;
 }
