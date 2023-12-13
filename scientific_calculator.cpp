a70ee9ec814016dcbe1bddc25d85fd525c7094e0
ShafiqHussain06032006 <147637693+ShafiqHussain06032006@users.noreply.github.com>
2023-12-13 11:32:00 +0500

    Add files via upload
    Dear Group Members 
    i just combined the code
    Arithematic with permutation and combination

--- scientific_calculator.cpp
+++ scientific_calculator.cpp
@@ -1,88 +1,133 @@
-//in this code we just make an arithematic operation
-#include<iostream>
+#include <iostream>
 using namespace std;
-double add(){
-cout<<"enter your numbers "<<endl;
-int a, b;
-cin>>a>>b;
-return a+b;
-}
-double subtract(){
-    int a, b;
-    cout<<"enter your numbers "<<endl;
-    cin>>a>>b;
-    return a-b;
-}
-double multiply(){
-     int a, b;
-    cout<<"enter your numbers "<<endl;
-    cin>>a>>b;
-    return a*b;
-}
-double divide(){
-    double a, b;
-    cout<<"enter your numbers "<<endl;
-    cin>>a>>b;
-    if (b==0)
-    {
-        cout<<"math error"<<endl;
-    }
-    return a/b;
-}
-int factorial(int n) {
+
+unsigned long long factorial(int n) {
     if (n == 0 || n == 1) {
         return 1;
     } else {
         return n * factorial(n - 1);
     }
 }
-int main()
-{
-     int operation;
-    do{
-    cout<<endl;
-   cout<<"enter 1 for addition "<<endl;
-   cout<<"enter 2 for subtraction "<<endl;
-   cout<<"enter 3 for multiplication "<<endl;
-   cout<<"enter 4 for division "<<endl;
-   cout<<"enter 5 for factorial "<<endl;
-   cout<<"enter 6 for exit"<<endl;
-   cin>>operation;
-   switch (operation)
-   {
-   case 1:
-    cout<<add();
-    break;
-   case 2:
-   cout<<subtract();
-   break;
-   case 3:
-   cout<<multiply();
-   break;
-   case 4:
-   cout<<divide();
-   break;
-   case 5:
-   int num;
-   cout << "Enter a non-negative integer: ";
-   cin >> num;
-    if (num < 0) {
-        
-        cout << "Please enter a non-negative integer." <<endl;
-    } else {
-       
-        unsigned long long result = factorial(num);
-        cout << "Factorial of " << num << " = " << result << endl;
+
+unsigned long long permutation(int n, int r) {
+    if (n < r) {
+        return 0;
     }
-    break;
-    case 6:
-    cout<<"existing the calculator";
-    break;
-   default:
-   cout<<"invslid please enter between 0-6";
-    break;
-   }}
-    
-    while(operation !=6);
+    return factorial(n) / factorial(n - r);
+}
+
+unsigned long long combination(int n, int r) {
+    if (n < r) {
+        return 0;
+    }
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+double add() {
+    cout << "Enter your numbers: " << endl;
+    int a, b;
+    cin >> a >> b;
+    return a + b;
+}
+
+double subtract() {
+    cout << "Enter your numbers: " << endl;
+    int a, b;
+    cin >> a >> b;
+    return a - b;
+}
+
+double multiply() {
+    cout << "Enter your numbers: " << endl;
+    int a, b;
+    cin >> a >> b;
+    return a * b;
+}
+
+double divide() {
+    cout << "Enter your numbers: " << endl;
+    double a, b;
+    cin >> a >> b;
+    if (b == 0) {
+        cout << "Math error" << endl;
+        return 0; // Returning 0 on division by zero
+    }
+    return a / b;
+}
+
+int main() {
+    int choice;
+    do {
+        cout << "\nEnter 1 for Arithmetic Operations" << endl;
+        cout << "Enter 2 for Permutation (nPr) and Combination (nCr)" << endl;
+        cout << "Enter 0 to exit" << endl;
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                int operation;
+                do {
+                    cout << "\nEnter 1 for addition" << endl;
+                    cout << "Enter 2 for subtraction" << endl;
+                    cout << "Enter 3 for multiplication" << endl;
+                    cout << "Enter 4 for division" << endl;
+                    cout << "Enter 5 for factorial" << endl;
+                    cout << "Enter 6 to go back to the main menu" << endl;
+                    cout << "Enter 0 to exit" << endl;
+                    cin >> operation;
+
+                    switch (operation) {
+                        case 1:
+                            cout << "Result: " << add() << endl;
+                            break;
+                        case 2:
+                            cout << "Result: " << subtract() << endl;
+                            break;
+                        case 3:
+                            cout << "Result: " << multiply() << endl;
+                            break;
+                        case 4:
+                            cout << "Result: " << divide() << endl;
+                            break;
+                        case 5:
+                            int num;
+                            cout << "Enter a non-negative integer: ";
+                            cin >> num;
+                            if (num < 0) {
+                                cout << "Please enter a non-negative integer." << endl;
+                            } else {
+                                unsigned long long result = factorial(num);
+                                cout << "Factorial of " << num << " = " << result << endl;
+                            }
+                            break;
+                        case 6:
+                            break; // Go back to the main menu
+                        case 0:
+                            cout << "Exiting..." << endl;
+                            return 0;
+                        default:
+                            cout << "Invalid choice. Please enter between 0-6" << endl;
+                            break;
+                    }
+                } while (operation != 6);
+                break;
+            case 2:
+                int n, r;
+                cout << "Enter value for n: ";
+                cin >> n;
+                cout << "Enter value for r: ";
+                cin >> r;
+                cout << "Permutation (nPr) is : " << permutation(n, r) << endl;
+                cout << "Combination (nCr) is : " << combination(n, r) << endl;
+                break;
+            case 0:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+        }
+    } while (choice != 0);
+
     return 0;
 }
